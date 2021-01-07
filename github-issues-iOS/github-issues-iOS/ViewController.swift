29048db1bddade56e474c7b7af6f96760cd4007a
Anurag Agnihotri <anurag.a@servify.in>
2021-01-07 15:49:16 +0530

    Added files for Xcode

---
+++ github-issues-iOS/github-issues-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-issues-iOS
+//
+//  Created by Anurag on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ github-issues-iOS/github-issues-iOSTests/github_issues_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  github_issues_iOSTests.swift
+//  github-issues-iOSTests
+//
+//  Created by Anurag on 07/01/21.
+//
+
+import XCTest
+@testable import github_issues_iOS
+
+class github_issues_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
